623509fdd5c01714f33db4f92d462b8df78bb599
Arnau Mora <arnyminerz@proton.me>
2023-05-25 10:18:20 +0200

    Adjusted theme
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,5 +1,4 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
